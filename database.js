0d34f683522606fb26f6b5e0253f89469ad37bb9
anna-jonsson <anna.f.jonsson@gmail.com>
2022-02-28 17:14:18 +0100

    Added elements to my database file (database.js)
    Created my JS-database containing 10 default cities.

---
+++ database.js
@@ -0,0 +1,83 @@
+//Database containing 10 default cities.
+
+let database = [
+    {
+        id: 1, 
+        name: "Stockholm",
+        country: "Sweden",
+        language: "Swedish",
+        visited: 2
+    }, 
+
+    {
+        id: 2, 
+        name: "Berlin", 
+        country: "Germany",
+        language: "German",
+        visited: 3
+    }, 
+
+    {
+        id: 3, 
+        name: "Madrid", 
+        country: "Spain",
+        language: "Spanish",
+        visited: 1
+    }, 
+
+    {
+        id: 4, 
+        name: "Oslo", 
+        country: "Norway",
+        language: "Norweigan",
+        visited: 4
+    }, 
+
+    {
+        id: 5, 
+        name: "New York", 
+        country: "USA",
+        language: "English",
+        visited: 0
+    }, 
+
+    {
+        id: 6, 
+        name: "Basel", 
+        country: "Switzerland",
+        language: "German, French",
+        visited: 6
+    }, 
+
+    {
+        id: 7, 
+        name: "Paris", 
+        country: "France",
+        language: "French",
+        visited: 3
+    }, 
+
+    {
+        id: 8, 
+        name: "Port Louis", 
+        country: "Mauritius",
+        language: "English, French",
+        visited: 1
+    }, 
+
+    {
+        id: 9, 
+        name: "Lisbon", 
+        country: "Portugal",
+        language: "Portuguese",
+        visited: 0
+    }, 
+
+    {
+        id: 10, 
+        name: "Montreal", 
+        country: "Canada",
+        language: "French, English",
+        visited: 0
+    }
+];
